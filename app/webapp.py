78a1eb207900ede7897e08f2a2fb96c853d7adb8
sami8999 <56681425+sami8999@users.noreply.github.com>
2020-12-03 20:50:07 +0000

    Update webapp.py

--- app/webapp.py
+++ app/webapp.py
@@ -58,10 +58,6 @@ def logout():
 
 @server_bp.route('/register/', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        #return redirect(url_for('main.index'))
-        return redirect ('https://samis-project.herokuapp.com/dashboard/')
-
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
