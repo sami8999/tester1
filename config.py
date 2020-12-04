8d305fab1b991383f8246ba23ece941374165403
sami8999 <56681425+sami8999@users.noreply.github.com>
2020-12-04 19:29:17 +0000

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY = 'kdbksdbkb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RECAPTCHA_USE_SSL= False
+    RECAPTCHA_PUBLIC_KEY= 'enter_your_public_key'
+    RECAPTCHA_PRIVATE_KEY='enter_your_private_key'
+    RECAPTCHA_OPTIONS = {'theme':'white'}
