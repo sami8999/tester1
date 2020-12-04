544778235e9b6e01149b0bba2fb1d511c6c14013
sami8999 <56681425+sami8999@users.noreply.github.com>
2020-12-04 00:06:52 +0000

    Update webapp.py

--- app/webapp.py
+++ app/webapp.py
@@ -38,7 +38,7 @@ def login():
             return render_template('login.html', form=form, error=error)
 
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
+        #next_page = request.args.get('next')
         #if not next_page or url_parse(next_page).netloc != '':
             #next_page = url_for('main.index')
         #return redirect(next_page)
